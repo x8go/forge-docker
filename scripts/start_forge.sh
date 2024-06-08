ebbc9dd7d7e41edc3e1b1d296a98974fd9761392
Ashley Kleynhans <ashley.kleynhans@gmail.com>
2024-06-08 21:51:14 +0200

    Bump base image to version 1.4.0 and don't auto update using git pull

--- scripts/start_forge.sh
+++ scripts/start_forge.sh
@@ -5,7 +5,6 @@ export HF_HOME="/workspace"
 VENV_PATH=$(cat /workspace/stable-diffusion-webui-forge/venv_path)
 source ${VENV_PATH}/bin/activate
 cd /workspace/stable-diffusion-webui-forge
-git pull
 nohup ./webui.sh -f > /workspace/logs/forge.log 2>&1 &
 echo "Stable Diffusion WebUI Forge started"
 echo "Log file: /workspace/logs/forge.log"
