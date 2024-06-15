354ca9d4ab2104a5649ba7b0c0532df40fea5a83
x8go <x8765xxx@gmail.com>
2024-06-15 09:29:42 +0900

    Update start_forge.sh

--- scripts/start_forge.sh
+++ scripts/start_forge.sh
@@ -2,9 +2,9 @@
 
 echo "Starting Stable Diffusion WebUI Forge"
 export HF_HOME="/workspace"
-VENV_PATH=$(cat /workspace/stable-diffusion-webui-forge/venv_path)
+VENV_PATH=$(cat /workspace/SD/venv_path)
 source ${VENV_PATH}/bin/activate
-cd /workspace/stable-diffusion-webui-forge
+cd /workspace/SD
 nohup ./webui.sh -f > /workspace/logs/forge.log 2>&1 &
 echo "Stable Diffusion WebUI Forge started"
 echo "Log file: /workspace/logs/forge.log"
